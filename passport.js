604165f2abe9eeb05d7f046713df23644e3a63af
jbettmann <jordan.bettmann@gmail.com>
2022-02-25 12:44:31 -0700

    Create passport.js and logic for JWT and Basic HTTP authentication

---
+++ passport.js
@@ -0,0 +1,50 @@
+
+
+const passport = require('passport'),
+  LocalStrategy = require('passport-local').Strategy,
+  Models = require('./models.js'),
+  passportJWT = require('passport-jwt');
+
+let Users = Models.User,
+  JWTStrategy = passportJWT.Strategy, 
+  ExtractJWT = passportJWT.ExtractJwt;
+
+// HTTP Authentication. Takes username and password from request body and usses Mongoose to check database
+passport.use(new LocalStrategy({
+  usernameField: 'Username',
+  passwordField: 'Password' // password doesnt not get check here. 
+  // if match, callback is executed. This is login endpoint
+}, (username, password, callback) => {
+  console.log(username + '  ' + password);
+  Users.findOne({ Username: username }, (error, user) => {
+    // if error occures
+    if (error) {  
+      console.log(error);
+      return callback(error);
+    }
+     // in no user is found, message passed to callback.
+    if (!user) {
+      console.log('incorrect username');
+      return callback(null, false, {message: 'Incorrect username or password.'});
+    }
+
+    console.log('finished');
+    return callback(null, user);
+  });
+}));
+
+// Authenticates users on JWT summited alongside request
+passport.use(new JWTStrategy({
+    // Extracts JWT from header of HTTP request.
+  jwtFromRequest: ExtractJWT.fromAuthHeaderAsBearerToken(),
+    // Verifies singature of JWT. Client is who it says it is.
+  secretOrKey: 'your_jwt_secret'
+}, (jwtPayload, callback) => {
+  return Users.findById(jwtPayload._id)
+    .then((user) => {
+      return callback(null, user);
+    })
+    .catch((error) => {
+      return callback(error)
+    });
+}));
