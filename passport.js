d700de5da66bbdd226590fac0a7dfc544bab2a4c
jbettmann <jordan.bettmann@gmail.com>
2022-02-26 13:51:31 -0700

    added password hashing validation

--- passport.js
+++ passport.js
@@ -27,7 +27,11 @@ passport.use(new LocalStrategy({
       console.log('incorrect username');
       return callback(null, false, {message: 'Incorrect username or password.'});
     }
-
+    // Hashes password enterd when logging in before comparing to password in database
+    if (!user.validatePassword(password)) {
+        console.log('incorrect password');
+        return callback(null, false, {message: 'Incorrect password.'});
+    }
     console.log('finished');
     return callback(null, user);
   });
