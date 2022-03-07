e96081b3f08c2464449afafc78a02beb51415dc2
jbettmann <jordan.bettmann@gmail.com>
2022-03-07 14:14:24 -0700

    allowed access to from all origins

--- index.js
+++ index.js
@@ -20,19 +20,20 @@ mongoose.connect(process.env.CONNECTION_URI || 'mongodb://localhost:27017/myFlix
 const app = express();
 
 // List of allowed domains
-let allowedOrigins = '*';
+let allowedOrigins = ['http://localhost:8080', 'http://testsite.com', 'http://localhost:1234'];
 
-app.use(cors({
-  origin: (origin, callback) => {
-    if(!origin) return callback(null, true);
-     // If a specific origin isn't found on the list of allowed origins
-    if(allowedOrigins.indexOf(origin) === -1) {
-      let message = `The CORS policy for this application doesn't all access from origin ${origin}`;
-      return callback(new Error(message), false);
-    }
-    return callback(null, true);
-  }
-}));
+app.use(cors());
+//   {
+//   origin: (origin, callback) => {
+//     if(!origin) return callback(null, true);
+//      // If a specific origin isn't found on the list of allowed origins
+//     if(allowedOrigins.indexOf(origin) === -1) {
+//       let message = `The CORS policy for this application doesn't all access from origin ${origin}`;
+//       return callback(new Error(message), false);
+//     }
+//     return callback(null, true);
+//   }
+// }));
 
 
 app.use(bodyParser.urlencoded({ extended: true }));
