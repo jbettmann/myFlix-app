5b9c439111488e5241ed505ff459643b8b427dcd
jbettmann <jordan.bettmann@gmail.com>
2022-03-07 14:35:16 -0700

    Update index.js

--- index.js
+++ index.js
@@ -22,17 +22,18 @@ const app = express();
 // List of allowed domains
 let allowedOrigins = ['http://localhost:8080', 'http://testsite.com', 'http://localhost:1234'];
 
-app.use(cors( {
-  origin: (origin, callback) => {
-    if(!origin) return callback(null, true);
-     // If a specific origin isn't found on the list of allowed origins
-    if(allowedOrigins.indexOf(origin) === -1) {
-      let message = `The CORS policy for this application doesn't all access from origin ${origin}`;
-      return callback(new Error(message), false);
-    }
-    return callback(null, true);
-  }
-}));
+app.use(cors());
+//   {
+//   origin: (origin, callback) => {
+//     if(!origin) return callback(null, true);
+//      // If a specific origin isn't found on the list of allowed origins
+//     if(allowedOrigins.indexOf(origin) === -1) {
+//       let message = `The CORS policy for this application doesn't all access from origin ${origin}`;
+//       return callback(new Error(message), false);
+//     }
+//     return callback(null, true);
+//   }
+// }));
 
 
 app.use(bodyParser.urlencoded({ extended: true }));
