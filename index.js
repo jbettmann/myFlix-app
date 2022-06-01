e6cbe10bf1749950657319b05e58a427dc30a138
jbettmann <jordan.bettmann@gmail.com>
2022-06-01 13:51:49 -0600

    Update index.js

--- index.js
+++ index.js
@@ -24,7 +24,7 @@ const app = express();
 let allowedOrigins = [
   "http://localhost:8080",
   "http://testsite.com",
-  "http://localhost:49899",
+  "http://localhost:1234",
   "http://localhost:4200",
   "https://bettsmyflix.netlify.app",
   "https://jbettmann.github.io",
